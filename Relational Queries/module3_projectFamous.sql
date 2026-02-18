f0c4f92a24793eee1f701accf18ec5ae8138e274
Stutie <stutiebanerjie@gmail.com>
2026-02-18 12:50:42 -0500

    Create module3_projectFamous.sql

---
+++ Relational Queries/module3_projectFamous.sql
@@ -0,0 +1,18 @@
+/*
+  Header
+  Name: Stutie Banerjie
+  Date: 2.18.2026
+  Project: Famous People
+*/
+
+/**
+    Instructions
+    > In this project, you’re going to make your own table with some small set of “famous people”, then make more tables about things they do and join those to create nice human readable lists.
+
+    > For example, here are types of famous people and the questions your data could answer:
+        Movie stars: What movies are they in? Are they married to each other?
+        Singers: What songs did they write? Where are they from?
+        Authors: What books did they write?
+        Fictional characters: How are they related to other characters? What books do they show up in?
+**/
+
